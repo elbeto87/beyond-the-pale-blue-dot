263fdccb453ef0e6d2cb18b559fe8c9a83b48520
Ezequiel Gresia <gresia.ezequiel@gmail.com>
2026-05-16 15:52:39 -0300

    refactor

---
+++ app/models/asteroid.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Column, String, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base import Base
+
+
+if TYPE_CHECKING:
+    from app.models.impact_event import ImpactEventModel
+
+
+class AsteroidModel(Base):
+    __tablename__ = "asteroids"
+    
+    asteroid_id: Mapped[str] = Column(String, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True, index=True)
+    estimated_diameter: Mapped[float | None] = mapped_column(Float, default=0.0)
+    absolute_magnitude_h: Mapped[float | None] = mapped_column(Float, default=0.0)
+    
+    impact_events: Mapped[list["ImpactEventModel"]] = relationship(back_populates="asteroid", cascade="all, delete-orphan")
---
+++ app/models/impact_event.py
@@ -0,0 +1,24 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base import Base
+from app.schemas.asteroid import AsteroidSchema
+
+
+if TYPE_CHECKING:
+    from app.models.asteroid import AsteroidModel
+
+
+class ImpactEventModel(Base):
+    __tablename__ = "impact_events"
+
+    impact_event_id: Mapped[str] = mapped_column(String, primary_key=True)
+    asteroid_id: Mapped[str] = mapped_column(ForeignKey("asteroids.asteroid_id"))
+    date: Mapped[str] = mapped_column(String)
+    impact_probability: Mapped[float] = mapped_column(Float)
+    energy: Mapped[float] = mapped_column(Float)
+    dangerous: Mapped[float] = mapped_column(Float)
+
+    asteroid: Mapped["AsteroidModel"] = relationship(back_populates="impact_event")
