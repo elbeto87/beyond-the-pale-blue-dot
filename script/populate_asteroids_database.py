92bc787b043b0ac7f2d75ab1014d68d72b677599
Ezequiel Gresia <gresia.ezequiel@gmail.com>
2026-05-21 07:47:57 -0300

    refactor

--- script/populate_asteroids_database.py
+++ script/populate_asteroids_database.py
@@ -1,5 +1,20 @@
+import httpx
+
+from app.clients.nasa_neo_ws_client import NASANeoWsClient
+from app.clients.nasa_sbdb_client import NASASbdbClient
+from app.clients.nasa_sentry_client import NASASentryClient
+from app.services.asteroid_service import AsteroidService
+
+
 def populate_asteroids_database():
-    print("hola")
+    with httpx.Client() as client:
+        asteroid_service = AsteroidService(
+            nasa_sbdb_client=NASASbdbClient(client),
+            nasa_neo_ws_client=NASANeoWsClient(client),
+            nasa_sentry_client=NASASentryClient(client),
+        )
+        results = asteroid_service.get_impact_data()
+        breakpoint()
 
 if __name__ == "__main__":
     populate_asteroids_database()
