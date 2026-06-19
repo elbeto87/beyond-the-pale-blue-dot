33591614904b9914afe1a5f87ba4a0232514a7ba
Ezequiel Gresia <gresia.ezequiel@gmail.com>
2026-06-19 18:15:17 -0300

    refactor

--- backend/script/populate_asteroids_database.py
+++ backend/script/populate_asteroids_database.py
@@ -35,7 +35,7 @@ def get_asteroid_basic_data(client: httpx.Client, asteroid_name: str) -> dict:
 
 
 
-def get_impact_event_data(client: httpx.Client, impact_probability: str = "1e-5"):
+def get_impact_event_data(client: httpx.Client, impact_probability: str = "1e-7"):
     params = {
         "all": 1,
         "ip-min": impact_probability,
