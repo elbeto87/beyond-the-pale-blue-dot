78168b9012aa209cc33f3d08bd1e21513658a613
Ezequiel Gresia <gresia.ezequiel@gmail.com>
2026-07-02 10:15:07 -0300

    refactor

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,10 @@ class Settings(BaseSettings):
     NASA_JPL_SBDB_BASE_URL: str = "https://ssd-api.jpl.nasa.gov/sbdb.api"
     NASA_NEO_WS_BASE_URL: str = "https://api.nasa.gov/neo/rest/v1/neo"
     NASA_API_KEY: str = "DEMO_KEY"
-    ALLOWED_ORIGINS: list[str] = ["http://localhost:5173"]
+    ALLOWED_ORIGINS: list[str] = [
+        "http://localhost:5173",
+        "https://the-end-of-the-world-ten.vercel.app",
+    ]
     DATABASE_URL: str
     PORT: int = 8000
     HOST: str = "0.0.0.0"
